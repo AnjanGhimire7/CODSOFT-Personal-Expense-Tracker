cfb4bae4f27520d71d33c6d8bd80831dac65f420
AnjanGhimire7 <anjanghimire876@gmail.com>
2024-07-14 22:15:01 +0545

    adding cashin and cashout function

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'package:flutter_application_2/constant/app_color.dart';
+import './constant/app_text.dart';
+import 'package:flutter_application_2/screen/dashboard.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var path = await getApplicationDocumentsDirectory();
+  Hive.init(path.path);
+  await Hive.openBox(AppText.hiveBox);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hive Demo',
+      theme: ThemeData(
+        primarySwatch: AppColors.primaryColor,
+      ),
+      home: const DashBoardScreen(),
+    );
+  }
+}
